df02891b403e5414814f864b3e7f9ae57c69edbd
Gator3000 <aelian.brd@proton.me>
2024-04-21 18:29:41 +0200

    starting database schema

--- dbconf.py
+++ dbconf.py
@@ -13,10 +13,42 @@ database = "accompagne"
 # Initialization
 def init(connexion):
     connexion.cursor.execute("""
-    CREATE TABLE drivingpupils
+    CREATE TABLE drivingschools (
+        id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        name varchar(50),
+        email varchar(50),
+        password varchar(256),
+        adress varchar(256),
+    )
     ;
     """)
     connexion.cursor.execute("""
-    CREATE TABLE drivingschools
+    CREATE TABLE drivingteachers (
+        id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        firstname varchar(20),
+        lastname varchar(30),
+        email varchar(50),
+        password varchar(256)
+        drivingschool int,
+        FOREIGN KEY(drivingschool) REFERENCES drivingschools (id)
+    )
+    ;
+    """)
+    connexion.cursor.execute("""
+    CREATE TABLE drivingpupils (
+        id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        firstname varchar(20),
+        lastname varchar(30),
+        email varchar(50),
+        password varchar(256)
+        drivingschool int,
+        FOREIGN KEY(drivingschool) REFERENCES drivingschools (id)
+    )
+    ;
+    """)
+    connexion.cursor.execute("""
+    CREATE TABLE route (
+
+    )
     ;
     """)
